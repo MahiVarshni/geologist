31925c1e2264d0169b3344d10154bcd9c0ed8e81
MahiVarshni <84715191+MahiVarshni@users.noreply.github.com>
2021-06-13 11:52:46 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,21 @@
+class Stone{
+	constructor(x,y,)
+	{
+	var options={
+     restitution:0.8,
+	 friction:0.9,
+	 density:12
+	}
+     this.body = Bodies.rectangle(x,y,50,120,options);
+		  this.width = 50;
+		  this.height = 120;
+		  World.add(world, this.body);
+	}
+	
+		display(){
+		  var pos =this.body.position;
+		  rectMode(CENTER);
+		  fill("black");
+		  rect(pos.x, pos.y, this.width, this.height);
+		}
+	  };
